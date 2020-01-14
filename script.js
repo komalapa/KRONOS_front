c600e47f2a903c196f342a98bcf28ddc8fecb2f9
komalapa <komalapa@mail.ru>
2020-01-14 18:48:20 +0400

    html css + vue.js serch str in JSON

---
+++ script.js
@@ -0,0 +1,75 @@
+const testJSONstr=`[
+    {
+        "locality-guid": "123456789",
+        "locality-name": "Москва",
+        "locality-type": "город",
+        "fedsubj-name": "Москва",
+        "fedsubj-type": "г.о.",
+        "time-zone": "3"
+    },
+    {
+        "locality-guid": "987654321",
+        "locality-name": "Самара",
+        "locality-type": "город",
+        "fedsubj-name": "Самарская",
+        "fedsubj-type": "обл.",
+        "time-zone": "4"
+    },
+    {
+        "locality-guid": "987654999",
+        "locality-name": "Сергеевка",
+        "locality-type": "деревня",
+        "fedsubj-name": "Самарская",
+        "fedsubj-type": "обл.",
+        "time-zone": "4"
+    }
+]`
+
+function findJSONPart(searchStr){
+    //console.log(testJSONstr);
+    let JSONforSearch = JSON.parse(testJSONstr);
+    //console.log(JSONforSearch);
+    //console.log("json 1 ln: "+JSONforSearch[1]["locality-name"]);
+    //console.log("indexof: "+JSONforSearch[1]["locality-name"].indexOf(searchStr));
+    //JSONforSearch.forEach(() =>{
+    //});
+    for(let i in JSONforSearch){
+        console.log("JS str: "+ JSONforSearch[i]["locality-name"]);
+        console.log("searchSTR: "+ searchStr)
+        console.log("indexof: "+JSONforSearch[i]["locality-name"].indexOf(searchStr));
+        if(JSONforSearch[i]["locality-name"].indexOf(searchStr)!=-1){
+            console.log("!!!!: "+JSONforSearch[i]["locality-name"]);
+            let findObj = Object.assign({}, JSONforSearch[i]);
+            return JSONforSearch[i];
+        } 
+      }
+}
+
+new Vue({
+    el: '#searchAPP',
+    data: {
+        searchStr:"Населенный пункт",
+        localityGuid: "",
+        localityName: "",
+        localityType: "",
+        fedsubjName: "",
+        fedsubjType: "",
+        timeZone: ""
+    },
+    methods:{
+        searchLocStr: function(){
+            //console.log("searchSTR: "+this.searchStr);
+            //console.log("search-result"+findJSONPart(this.searchStr));
+            
+            let found = findJSONPart(this.searchStr);
+            this.localityGuid=found["locality-guid"];
+            this.localityName=found["locality-name"];
+            this.localityType=found["locality-type"];
+            this.fedsubjName=found["fedsubj-name"];
+            this.fedsubjType=found["fedsubj-type"];
+            this.timeZone="GMT +"+found["time-zone"];
+
+            console.log(this);
+        }
+    }
+  })
